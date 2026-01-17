3df66e8c99538b4e9b2893df16a592a9565cf00a
Alessio Galtelli <alessiogaltelli@yahoo.com>
2026-01-17 23:28:45 +0100

    added eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+// eslint.config.js
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      // add your rules here
+    },
+  },
+];
